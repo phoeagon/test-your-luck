07148282e5cd969f7e3f407c6aa4cce77fca128d
phoeagon <phoeagon@gmail.com>
2013-07-24 21:41:09 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-test-your-luck
+Test-Your-Luck
 ==============
 
+phoeagon, 2013 July 24
+
+
 test-your-luck, aka Renpin Tester. An innocent joke only.
+
+For my times in senior high school.
---
+++ RP/Unit2.pas
@@ -0,0 +1,36 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TForm2 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  close;
+end;
+
+end.
